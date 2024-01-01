9e4885b234cc3d41b4a3ad562654c02122d67c10
Vivek <vivekpatidar549@gmail.com>
2024-01-01 21:46:32 +0530

    SignUp Completed

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,6 +1,8 @@
 const User=require('../models/user');
 const OTP=require('../models/OTP');
 const otpGenerator=require('otp-generator');
+const Profile=require('../models/Profile');
+require('bcrypt');
 
 // sendOTP
 exports.sendOTP=async(req,res)=>{
@@ -54,7 +56,69 @@ exports.sendOTP=async(req,res)=>{
 
 
 //sign up
-
+exports.signUp=async(req,res)=>{
+    try{
+        //data fetch
+        const{firstName,lastName,email,password,confirmPassword,accountType,contactNumber,otp}=req.body;
+        //validate data
+        if(!firstName || !lastName || !email|| !password|| !confirmPassword|| !otp){
+            return res.status(403).json({
+                success:false,
+                message:"All fields are required",
+            })
+        }
+        // match password
+        if(password !==confirmPassword){
+            return res.status(400).json({
+                success:false,
+                message:"Passowrd does not match",
+            })
+        }
+        // check user exits or not
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"User already registered",
+            })
+        }
+        // find most recent otp stored from db
+        const recentOtp=await OTP.find({email}).sort({createdAt:-1}).limit(1);
+        console.log(recentOtp);
+        // createdAt:-1  This is the sorting criterion. It indicates that the documents should be sorted based on the createdAt field in descending order, 
+        // means we will get most recent entry and also limit(1):-  This limits the number of documents returned to just one.
+        // validate otp
+        if(recentOtp.length==0){
+            return res.status(400).json({
+                success:false,
+                message:"OTP Not Found",
+            })
+        }
+        else if(otp!==recentOtp.otp){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid OTP",
+            })
+        }
+        // hash passowrd
+        const hashedPassword=await bcypt.hash(password,10);
+        // create entry in db
+        const profileDetails=await Profile.create({gender:null,dateOfBirth:null,about:null,contactNumber:null});
+        const user=await User.create({firstName,lastName,email,password:hashedPassword,accountType,contactNumber,additionalDetails:profileDetails._id,image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstname} ${lastname}`});
+        //return res
+        return res.status(200).json({
+            success:true,
+            message:"User Registered successfully",
+            user,
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"User can not be registered Please try again",
+        })
+    }
+}
 
 // login 
 
