cae316b8189fad341015259ae79eb00a4d56a86b
Vivek <vivekpatidar549@gmail.com>
2024-01-01 23:20:41 +0530

    reset password completed

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -1,6 +1,7 @@
+const user = require('../models/user');
 const User=require('../models/user');
 const mailSender=require('../utilities/mailSender');
-
+const bcrypt=require('bcrypt');
 
 
 
@@ -47,11 +48,47 @@ exports.resetPasswordToken=async(req,res)=>{
 // reset password
 
 exports.resetPassword=async(req,res)=>{
+    try{
     // data fetch
+    const {password,confirmPassword,token}=req.body;
     //validation
+    if(password!==confirmPassword){
+        return res.json({
+            success:false,
+            message:"Password not matching",
+        })
+    }
     // get userdetails from db using token
+    const userDetails=await User.findOne({token:token});
+    if(!userDetails){
+        return res.json({
+            success:false,
+            message:"Token invalid",
+        })
+    }
+    if(userDetails.resetPasswordExpires<Date.now()){
+        return res.json({
+            success:false,
+            message:"Request time out, TOken expired regenate token",
+        })
+    }
     // if no entry ->invalid token / or token expires
     //hash password
+    const hashpassword=await bcrypt.hash(password,10);
     //update password
+    await User.findOneAndUpdate({token},{
+        password:hashpassword,
+    },{new:true}) // this line return the new updated document
     //return response
+    return res(200).json({
+        success:true,
+        message:"Password reset Successfull",
+    })
+    }catch(error){
+        return res.json({
+            success:false,
+            message:"Request time out, TOken expired regenate token",
+        })
+    }
+    
 }
