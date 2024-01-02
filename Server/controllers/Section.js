3e7dc842df29dddeecf0946cfcdc83053f164979
Vivek <vivekpatidar549@gmail.com>
2024-01-02 13:47:15 +0530

    update section Completed

--- Server/controllers/Section.js
+++ Server/controllers/Section.js
@@ -1,3 +1,70 @@
+const Section=require('../models/Section');
+const Course=require('../models/Course')
+
 //create
+exports.createSection=async(req,res)=>{
+    try{
+        //data fetch
+        const {sectionName,courseId}=req.body;
+        //data validation
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success:false,
+                message:"Missing Properties",
+            })
+        }
+        //create section
+        const newSection=await Section.create({sectionName});
+        // update course
+        //TODO :- how can i populate section and subsection
+        const updatedCourseDetails=await Course.findByIdAndUpdate(courseId,
+            {
+                $push:{
+                    courseContent:newSection._id,
+                }
+            
+        },{new:true});
+        // use populate to replace section/subsection both in the updatecoursedetails
+        return res.status(200).json({
+            success:true,
+            message:"Section Created SuccessFully",
+            updatedCourseDetails,
+        })
+        
+        
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Unable to Create section",
+        })
+    }
+}
 //update
+
+exports.updateSection=async(req,res)=>{
+    try{
+        //data fetch
+        const {sectionName,sectionId}=req.body;
+        //data validation
+        if(!sectionName || !sectionId){
+            return res.status(400).json({
+                success:false,
+                message:"Missing Properties",
+            })
+        }
+        //update data
+        const section=await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Section updated SuccessFully",
+            updatedCourseDetails,
+        })
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Unable to Update Section",
+        })
+    }
+}
 //delete
