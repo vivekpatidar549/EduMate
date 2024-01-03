c21c5885482bfbc38478f3cb444eebc8642a02c5
Vivek <vivekpatidar549@gmail.com>
2024-01-03 08:54:37 +0530

    Average rating done

--- Server/controllers/RatingAndReview.js
+++ Server/controllers/RatingAndReview.js
@@ -1,5 +1,6 @@
 const RatingAndReview= require('../models/RatingAndReview');
 const Course=require('../models/Course');
+const { default: mongoose } = require('mongoose');
 
 
 //create rating
@@ -29,17 +30,62 @@ exports.createRating=async(req,res)=>{
         //create rating and review
         const ratingReview =await RatingAndReview.create({rating,review,course:courseId,user:userId});
         //update course with this rating and review
-        await Course.findByIdAndUpdate(courseId,{
+        const updatedCourseDetails=await Course.findByIdAndUpdate(courseId,{
             $push:{
                 ratingAndReviews:ratingReview._id,
             }
         },{new:true});
+        console.log(updatedCourseDetails);
         //return response
         return res.status(200).json({
             success:false,
             message:"Rating Review Created Successfully",
         })
 
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Error While Creating rating",
+        })
+    }
+}
+
+
+
+
+// get avg rating
+
+exports.getAverageRating=async(req,res)=>{
+    try{
+        // get course id
+        const courseId=req.body.courseId;
+        // calculate avg rating
+        const result= await RatingAndReview.aggregate([
+            {
+                $match:{
+                    course:new mongoose.Types.ObjectId(courseId),
+                },
+            },{
+                $group:{
+                    _id:null,  // no criteria to group
+                    averageRating:{$avg:"$rating"},   // finding average
+                }
+            }
+        ])
+
+        if(result.length>0){
+            return res.status(200).json({
+                success:true,
+                averageRating:result[0].averageRating,
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            message:"Average rating is 0, no rating is given till now",
+            averageRating:0,
+        })
+
+        //return avg rating
     }catch(error){
         return res.status(500).json({
             success:false,
